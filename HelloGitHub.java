3493aab4fd6e836a6fcb36bec74356d4d8f1d0c0
Alex Jacoby <alex.jacoby@dc.gov>
2018-08-17 10:19:32 -0400

    Create HelloGitHub.java

---
+++ HelloGitHub.java
@@ -0,0 +1,6 @@
+public class HelloGitHub {
+  public static void main(String[] args) {
+    // TODO: Correct the name in the next line and submit your changes.
+    System.out.println("Hello, world!  My name is Grace Hopper!");
+  }
+}
